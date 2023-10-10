75fcb74ba8dbe5488aadc65fdd847e45765c1649
zeinali.b69 <zeinali_bahareh@yahoo.com>
2023-10-10 14:20:22 +0330

    fix colorMode bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-
 import NavBar from "./component/NavBar";
 import GameGrid from "./component/GameGrid";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+
+  // Create a Material-UI theme with dark or light mode
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? "dark" : "light",
+    },
+  });
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
   return (
-    <div className="app">
-      <div className="content">
-        <NavBar />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="app">
+        <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
         <GameGrid />
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -10,9 +10,12 @@ import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import Box from "@mui/material/Box";
 import logo from "../assets/resource/Logo/logo.webp";
 
-const NavBar = () => {
-  const [darkMode, setDarkMode] = useState<boolean>(false);
+interface Props {
+  darkMode: boolean;
+  toggleDarkMode: () => void;
+}
 
+const NavBar = ({ darkMode, toggleDarkMode }: Props) => {
   // Create a Material-UI theme with dark or light mode
   const theme = createTheme({
     palette: {
@@ -20,28 +23,21 @@ const NavBar = () => {
     },
   });
 
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
-
   return (
-    <div>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Box display="flex" justifyContent="space-between">
-          <img src={logo} width="60" />
-          <Box display="flex">
-            <IconButton onClick={toggleDarkMode}>
-              {theme.palette.mode === "dark" ? (
-                <LightModeOutlinedIcon />
-              ) : (
-                <DarkModeOutlinedIcon />
-              )}
-            </IconButton>
-          </Box>
+    <>
+      <Box display="flex" justifyContent="space-between">
+        <img src={logo} width="60" />
+        <Box display="flex">
+          <IconButton onClick={toggleDarkMode}>
+            {theme.palette.mode === "dark" ? (
+              <LightModeOutlinedIcon />
+            ) : (
+              <DarkModeOutlinedIcon />
+            )}
+          </IconButton>
         </Box>
-      </ThemeProvider>
-    </div>
+      </Box>
+    </>
   );
 };
 
